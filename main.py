245012692a53683ec87e21927e0a42d0234e058e
Luke <88560297+accompts@users.noreply.github.com>
2023-08-16 01:50:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import sys
+import requests
 #
 #
 #
